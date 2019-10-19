6bdbfaee08200dde74cd14b13ec6e14429dc3d31
Eugene Kozlov <kozlovea8@gmail.com>
2019-10-19 15:37:56 +0300

    Update insertion sort implementation in python 3
    Shortand version

--- algorithms/sort/insertion/python.py
+++ algorithms/sort/insertion/python.py
@@ -4,28 +4,16 @@ import random as rnd;
 def insertion_sort(a):
     for j in range(1, len(a)):
         insert_element(a[j], a, j - 1)
+        print(a)
 
 
 # Вставить элемент e в массив a с учётом того что элементы a от 0 до i упорядочены
 def insert_element(e, a, i):
-    index = find_index(e, a, i)
+    while(i >= 0 and a[i] > e):
+        a[i + 1] = a[i]
+        i -= 1
 
-    shift(a, index, i)
-    a[index] = e
-
-
-def find_index(e, a, i):
-    j = 0
-    while a[j] < e:
-        j += 1
-
-    return j
-
-
-# Сдвинуть вправо
-def shift(a, begin, end):
-    for j in range(end + 1, begin, -1):
-        a[j] = a[j - 1]
+    a[i + 1] = e
 
 
 def create_random_array(n):
@@ -36,7 +24,7 @@ def create_random_array(n):
 
 
 if __name__ == '__main__':
-    array = create_random_array(20)
+    array = [5,2,4,6,1,3]
     print(array)
     insertion_sort(array)
     print(array)
